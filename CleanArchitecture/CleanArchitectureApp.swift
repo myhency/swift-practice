5b5cc74c5a5d1db6c2ea98622230b4cde3f7e6cb
myhency <hency.yeo@gmail.com>
2023-03-28 14:23:39 +0900

    Initial Commit

---
+++ CleanArchitecture/CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureApp.swift
+//  CleanArchitecture
+//
+//  Created by James on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
