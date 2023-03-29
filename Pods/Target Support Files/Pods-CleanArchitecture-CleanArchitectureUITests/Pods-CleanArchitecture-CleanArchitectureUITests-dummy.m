787f642fb7e6de9b49c43c6e5bf2053567b1a040
myhency <hency.yeo@gmail.com>
2023-03-29 12:32:07 +0900

    .gitignore 생성

--- Pods/Target Support Files/Pods-CleanArchitecture-CleanArchitectureUITests/Pods-CleanArchitecture-CleanArchitectureUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CleanArchitecture_CleanArchitectureUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CleanArchitecture_CleanArchitectureUITests
-@end
--- Pods/Target Support Files/Pods-CleanArchitectureTests/Pods-CleanArchitectureTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CleanArchitectureTests : NSObject
-@end
-@implementation PodsDummy_Pods_CleanArchitectureTests
-@end
